7f04fd8b3e436749358834116332c52fb2b9e2c1
Sukant Gupta <guptasukant2@gmail.com>
2024-03-18 22:14:17 +0530

    Added the handlers for password check, upload endpoint to acces the file along with its name, path and password. Route to get the file with a string id of the specific file. function to find the file in the db via the file id and checking whether password matches or not. Downloading the file and saving it.

--- index.js
+++ index.js
@@ -3,27 +3,70 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
+import File from "./models/file.js";
 dotenv.config();
-const app = express();
 
 const upload = multer({ dest: "uploads" });
 
 mongoose.connect(process.env.MONGO_URL);
 
+const app = express();
+
 app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
 	res.render("index");
 });
 
-app.post("/upload", upload.single("file"), (req, res) => {
+app.post("/upload", upload.single("file"), async (req, res) => {
 	const fileData = {
 		path: req.file.path,
 		originalName: req.file.originalname,
 	};
-	if ((req.body.password !== null) & (req.body.password !== "")) {
-		fileData.password = bcrypt.hash(req.body.password, 10);
+	if (req.body.password !== null && req.body.password !== "") {
+		fileData.password = await bcrypt.hash(req.body.password, 10);
+	} else {
+		fileData.password = null;
 	}
+	// $ Create a new file in the database
+	const file = await File.create(fileData);
+	console.log(file);
+
+	res.render("index", { fileLink: `${req.headers.origin}/file/${file._id}` });
 });
+
+// $ Route to get the file with a string id of the specific file.
+app
+	.route("/file/:id")
+	.get(handleDownload)
+	.post(handleDownload);
+// app.get("/file/:id", handleDownload)
+// app.post("/file/:id", handleDownload)
+
+async function handleDownload(req, res) {
+	// res.send(req.params.id);
+	const downloadFile = await File.findById(req.params.id);
+
+	if (downloadFile.password != null) {
+		if (req.body.password == null) {
+			res.render("password");
+			return;
+		}
+
+		if (!(await bcrypt.compare(req.body.password, downloadFile.password))) {
+			res.render("password", { error: true });
+			return;
+		}
+	}
+
+	downloadFile.downloadCount++;
+	// $ Save the file to the database
+	await downloadFile.save();
+	console.log(downloadFile.downloadCount, downloadFile.originalName);
+	// $ Download the file
+	res.download(downloadFile.path, downloadFile.originalName);
+}
 
 app.listen(process.env.PORT);
